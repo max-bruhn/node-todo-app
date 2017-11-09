541fb4abbd32882b5628f60c4459db2ae3d002a9
Max Bruhn <mpcbruhn@gmail.com>
2017-11-09 18:09:01 +0700

    add GET todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.post('/todos', (req, res) => {
     });
 });
 
+// list resources video
+app.get('/todos/', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, () =>{
     console.log('startet on port 3000');
 } );
